0d88a8c2d9042070b9840b92f34b76553f8d23b0
Danilomr305 <danilomr305@gmail.com>
2023-06-28 22:31:22 -0300

    ,

--- gymapp_live/lib/main.dart
+++ gymapp_live/lib/main.dart
@@ -39,8 +39,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title)),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
