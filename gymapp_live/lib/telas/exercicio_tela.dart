260b5a924ec1468ea70e2d2775d4d44860ab988e
Danilomr305 <danilomr305@gmail.com>
2023-06-29 13:56:16 -0300

    j

--- gymapp_live/lib/telas/exercicio_tela.dart
+++ gymapp_live/lib/telas/exercicio_tela.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class ExercicoTela extends StatelessWidget {
+  const ExercicoTela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
